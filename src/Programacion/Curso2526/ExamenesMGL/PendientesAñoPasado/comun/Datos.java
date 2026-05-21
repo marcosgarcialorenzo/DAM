f4a40680f189aecd8271f6822060c56131a00091
Marcos García Lorenzo <marcos.garcialorenzo@alumno.iesluisvives.org>
2026-05-21 18:16:21 +0200

    feat: renombrar paquetes y archivos de exámenes para mejorar la organización

--- src/Programacion/Curso2526/ExamenesMGL/PendientesAñoPasado/comun/Datos.java
+++
@@ -1,7 +0,0 @@
-package Programacion.Curso2526.ExamenesMGL.PendientesAñoPasado.comun;
-
-public class Datos {
-    public final static String NOMBRETABLAMEDICOS = "Medicos";
-    public static final String NOMBREFICHERO = "pacientes.txt";
-    public static final int TIEMPOPORPACIENTE = 15; //minutos
-}
