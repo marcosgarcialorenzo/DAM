99c0bf0096a3e4d7c5ca6ee3d74bf62c35641cbf
Marcos García Lorenzo <marcos.garcialorenzo@alumno.iesluisvives.org>
2026-09-15 10:49:37 +0200

    feat: agregar clase Main para inicializar la aplicación y mostrar el menú

---
+++ src/Curso2627/AccesoADatos/Tema1/Ejercicio1/Main.java
@@ -0,0 +1,9 @@
+package Curso2627.AccesoADatos.Tema1.Ejercicio1;
+
+public class Main {
+    public static void main(String[] args) {
+        GestorDatos gestorDatos = new GestorDatos();
+        Menu menu = new Menu(gestorDatos);
+        menu.mostrarMenu();
+    }
+}
